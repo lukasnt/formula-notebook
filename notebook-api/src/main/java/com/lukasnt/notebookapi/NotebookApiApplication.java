304491cfa7349dbb0fa0ca9d2d05cb56645f844a
Lukas Tveiten <lukastveiten@hotmail.no>
2025-12-15 19:46:56 +0100

    Add notebook-api project and run spring initializer

---
+++ notebook-api/src/main/java/com/lukasnt/notebookapi/NotebookApiApplication.java
@@ -0,0 +1,13 @@
+package com.lukasnt.notebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookApiApplication.class, args);
+    }
+
+}
---
+++ notebook-api/src/test/java/com/lukasnt/notebookapi/NotebookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukasnt.notebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
