c916df1ccea181399824e8b0d67990b6a192719a
Lukas Tveiten <lukastveiten@hotmail.no>
2025-12-18 09:26:38 +0100

    Add material-ui dependencies and remove tailwindcss dependencies

--- notebook-ui/app/routes/home.tsx
+++ notebook-ui/app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import {Paper} from "@mui/material";
 
 export function loader() {
   return { name: "React Router" };
@@ -6,14 +7,12 @@ export function loader() {
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="text-center p-4">
-      <h1 className="text-2xl">Hello, {loaderData.name}</h1>
-      <a
-        className="block mt-2 text-blue-500 underline hover:text-blue-600"
-        href="https://reactrouter.com/docs"
-      >
-        React Router Docs
-      </a>
+    <div className="home">
+        <Paper variant="outlined">
+            <div>
+                Lore ipsum
+            </div>
+        </Paper>
     </div>
   );
 }
