655c5d7de70382dd0736de4147291df3783e04a7
Lukas Tveiten <lukastveiten@hotmail.no>
2025-12-21 09:09:08 +0100

    Add flyway dependency and create initial database tables

---
+++ notebook-api/src/main/resources/db/migration/V1__create_notebooks_cells.sql
@@ -0,0 +1,20 @@
+CREATE TABLE notebooks (
+    notebook_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    created TIMESTAMP NOT NULL,
+    modified TIMESTAMP
+);
+
+CREATE TABLE cells (
+    cell_id SERIAL,
+    notebook_id INTEGER,
+    PRIMARY KEY (cell_id, notebook_id),
+    CONSTRAINT fk_notebook_id
+        FOREIGN KEY (notebook_id)
+        REFERENCES notebooks (notebook_id)
+        ON DELETE CASCADE,
+    symbol VARCHAR(32),
+    updated TIMESTAMP NOT NULL,
+    text_content VARCHAR,
+    evaluated DECIMAL
+);
