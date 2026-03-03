5a06f4a7aea0240c2e24810511e5d2821fcfeec7
Lukas Tveiten <lukastveiten@hotmail.no>
2026-03-03 09:59:58 +0100

    Test github actions workflow

--- notebook-api/src/test/java/com/lukasnt/notebookapi/NotebookApplicationTests.java
+++ notebook-api/src/test/java/com/lukasnt/notebookapi/NotebookApplicationTests.java
@@ -1,10 +1,18 @@
 package com.lukasnt.notebookapi;
 
+import com.lukasnt.notebookapi.config.FlywayConfig;
+import com.lukasnt.notebookapi.database.NotebookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class NotebookApplicationTests {
+
+    @MockitoBean
+    FlywayConfig flywayConfig;
 
     @Test
     void contextLoads() {
